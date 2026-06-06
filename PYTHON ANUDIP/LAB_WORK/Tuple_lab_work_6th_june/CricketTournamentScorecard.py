daf9c068ce8b491c1f31a6593a89f77584b03ae6
deepanshuRajput91134 <dc7064695@gmail.com>
2026-06-06 11:47:27 +0530

    Cricket Tournament Scorecard

---
+++ PYTHON ANUDIP/LAB_WORK/Tuple_lab_work_6th_june/CricketTournamentScorecard.py
@@ -0,0 +1,30 @@
+# Cricket Tournament Scorecard
+
+scores = [45, 78, 12, 100, 67, 8, 90, 55]
+
+half_century = 0
+century = 0
+
+for score in scores:
+    if score >= 50 and score < 100:
+        half_century += 1
+    elif score >= 100:
+        century += 1
+
+print("Half Centuries:", half_century)
+print("Centuries:", century)
+
+# Highest score
+print("\nHighest Score:", max(scores))
+
+# Scores below 20
+print("\nScores below 20:")
+
+for score in scores:
+    if score < 20:
+        print(score)
+
+# Average score
+average = sum(scores) / len(scores)
+
+print("\nAverage Score:", average)
