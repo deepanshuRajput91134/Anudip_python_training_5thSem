45c51f767cdb7d41336bcdf999d0389087294cb1
deepanshuRajput91134 <dc7064695@gmail.com>
2026-06-09 23:51:14 +0530

    Create menu-driven program for 2D shape operations

---
+++ PYTHON ANUDIP/CLASS_WORK/functionPython_9th_june/mensuration.py
@@ -0,0 +1,16 @@
+# function to calculate area of rectangle
+
+def area_rectangle(length, breadth):
+
+    area = length * breadth
+
+    return area
+
+
+# function to calculate perimeter of rectangle
+
+def perimeter_rectangle(length, breadth):
+
+    perimeter = 2 * (length + breadth)
+
+    return perimeter
