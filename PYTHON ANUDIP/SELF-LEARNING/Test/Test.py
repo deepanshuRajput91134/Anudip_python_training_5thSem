7a31635f55dcc1e578cf0a8c3f701c51fe620779
deepanshuRajput91134 <dc7064695@gmail.com>
2026-06-12 12:03:34 +0530

    Add Hospital Patient Monitoring System using dictionary analysis

---
+++ PYTHON ANUDIP/SELF-LEARNING/Test/Test.py
@@ -0,0 +1,5 @@
+#Write a program to input 8 numbers into a list, convert them into a set, and display the unique elements in ascending order.
+numbers=[]
+for i range is(8);
+num=int(input(Enter number(i+1))):
+    print("Enter the number")
